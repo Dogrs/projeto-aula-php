4d48832bcd51b17d45c0e84248d4617e8a365f14
Luiz Motter <luiz.motter@gmail.com>
2018-07-16 19:38:05 -0300

    Commit do arquivo do banco de dados

---
+++ banco_de_dados.sql
@@ -0,0 +1,21 @@
+CREATE TABLE pessoa (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(255) DEFAULT NULL,
+    email varchar(255) DEFAULT NULL,
+    sexo varchar(1) DEFAULT NULL,
+    data_nascimento timestamp NOT NULL,
+    uf varchar(255) DEFAULT NULL,
+    cidade varchar(255) DEFAULT NULL
+)
+
+CREATE TABLE uf(
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(255) DEFAULT NOT NULL,
+    sigla varchar(2) DEFAULT NOT NULL
+)
+
+CREATE TABLE cidade(
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(255) DEFAULT NOT NULL,
+    uf_id varchar(2) DEFAULT NOT NULL
+)
