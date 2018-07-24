9a45fa040ace15fa1693774a144ac3678a53c46d
Luiz Motter <luiz.motter@gmail.com>
2018-07-23 21:45:32 -0300

    Adicionado Cadastro de Cidades

--- modulo-cidade/cadastro-cidade.php
+++ modulo-cidade/cadastro-cidade.php
@@ -20,33 +20,12 @@ function validarFormularioSimples($post)
         $listaErros['nome'] = "Nome obrigatório.";
     }
 
-    if (!$post['email']) {
-        $listaErros['email'] = "Email obrigatório.";
-    } else if ( !validarEmail($post['email']) ) {
-        $listaErros['email'] = "Informe um email válido.";
-    }
 
-    if (!$post['sexo']) {
-        $listaErros['sexo'] = "Selecione um sexo.";
-
-    } else if ( !in_array($post['sexo'], ['M', 'F']) ) {
-        // o IF acima equivale ao IF comentado abaixo
-        // if ($post['sexo'] != 'M' && $post['sexo'] != 'F' )
-        $listaErros['sexo'] = "Selecione Masculino ou Feminino.";
-    }
-    
-    if (!$post['data_nascimento']) {
-        $listaErros['data_nascimento'] = "Data de nascimento obrigatória.";
-    }
-
-    if (!$post['uf']) {
+    if (!isset ($post['uf'])  || !$post['uf']){
         $listaErros['uf'] = "Estado obrigatório.";
     }
 
-    if (!$post['cidade']) {
-        $listaErros['cidade'] = "Cidade obrigatória.";
-    }
-
+   
     return $listaErros;
 }
 
@@ -71,11 +50,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     if (count($listaErros) > 0) {
         include "cadastro-view.php";
-    }
-    
+     }else {
+
+        //dd($_POST); //Teste para ver oque está enviando
+        $sql = "INSERT INTO cidade (nome,uf_id) 
+        VALUES('".$_POST['nome']."', ".$_POST['uf'] .");";
+
+        //dd($sql); // Teste para ver o resultado do SQL que será enviado.
+
+        $cidadeId = insert_db($sql); // chama a função que grava no banco   
+        $mensagemSucesso = '';
+        $mensagemErro = '';
+        //dd($cidade_id); // testa resultado após inserir
+
+            
+        if ($cidadeId) {
+            $mensagemSucesso= "Cidade cadastrada com sucesso";
+        } else{
+            $mensagemErro = "Erro Inesperado";
+        }
+
+        include "cadastro-view.php";
+     }
+
+
+
+    /*
     echo $_POST['nome'];
     echo "<br>";
-    /*
     echo $_POST['email'];
     echo '<br>';
     echo $_POST['sexo'];
